dc50ddab59b174aeef27c591b6923213af5cbd97
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2022-08-20 11:14:22 +0800

    第二次：完成教程4，使用generic.ListView和generic.DetailView

--- polls/urls.py
+++ polls/urls.py
@@ -7,14 +7,14 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex:/polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # 如果你想改变投票详情视图的 URL，比如想改成 polls/specifics/12/ ，你不用在模板里修改任何东西（包括其它模板）
     # ，只要在 polls/urls.py 里稍微修改一下就行：
     # path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex:/polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex:/polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
--- polls/views.py
+++ polls/views.py
@@ -2,61 +2,28 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-# 第一种方式
-def index1(request):
-    """第一种方式"""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    # output = ','.join([q.question_text for q in latest_question_list])
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """返回最新的5个发布的问题"""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-    """第二种 快捷方式"""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def detail_1(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def detail_2(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exists')
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results0(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-
-def vote0(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -64,7 +31,7 @@ def vote(request, question_id):
     # print(dir(request))
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoseNotExist):
+    except (KeyError, Choice.DoesNotExist):
         # 重新显示问题投票表。
         return render(request, 'polls/detail.html', {
             'question': question,
