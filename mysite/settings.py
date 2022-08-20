515351ac66eeb0b929392d933f8d77a9f3b12a16
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2022-08-20 18:00:32 +0800

    第二次：完成教程7结束，自定义后台表单 添加关联的对象 自定义后台更改列表 自定义后台界面和风格 自定义后台主页 接下来要做什么？

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # 设置后台模板
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,50 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
+
 
 # Register your models here.
-admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#
+#
+# admin.site.register(Question, QuestionAdmin)
+#
+#
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+
+
+# class ChoiceInline(admin.StackedInline):
+#     """堆叠式，宽松"""
+#     model = Choice
+#     extra = 3
+
+class ChoiceInline(admin.TabularInline):
+    """内联式，紧凑"""
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]  # 内部行
+    # 列表页显示详情
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # 增加过滤器
+    list_filter = ['pub_date']
+    # 增加一个搜索框
+    search_fields = ['question_text']
+    # 分页器
+    list_per_page = 2
+
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
